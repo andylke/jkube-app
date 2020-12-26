6601f517e18d51ac9ea335bc134372073b6d274e
Andy Lian <andylke@gmail.com>
2020-12-26 10:09:04 +0800

    Initial commit.

---
+++ src/test/java/io/getstarted/spring/jkubeapp/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.getstarted.spring.jkubeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
