000a30d9670a415ff91310a0031387d86c59739d
Andy Lian <andylke@gmail.com>
2021-01-13 09:29:32 +0800

    transfer ownership to io.github.andylke

--- src/main/java/io/github/andylke/jkubeapp/DemoApplication.java
+++ src/main/java/io/github/andylke/jkubeapp/DemoApplication.java
@@ -1,4 +1,4 @@
-package io.getstarted.spring.jkubeapp;
+package io.github.andylke.jkubeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
